e955e60176fd9c4342797140c8289a61ae4a5bd1
trevkat <trevor.katilius@icloud.com>
2024-01-02 11:07:09 -0600

    Testing JS Loops

---
+++ JS Loops/test.js
@@ -0,0 +1,49 @@
+// If Statement:
+
+var youLikeMeat = true;
+var myNum = 10;
+
+
+/*
+if (youLikeMeat) {
+
+    document.write("Here is the meaty menu....");
+
+} else {
+
+    document.write("You hate meat!");
+}
+
+
+
+
+if (myNum == 9) {
+
+    document.write("myNum is equal to 10");
+} else {
+
+    document.write("myNum is NOT equal to 10");
+}
+
+*/
+
+// Else If statement:
+
+var myAge = 29;
+
+if (myAge > 30) {
+    
+    document.write("You are over 30!");
+    
+} else if (myAge > 20){
+    
+    document.write("You are over 20!");
+    
+} else if (myAge > 10){
+    
+    document.write("You are over 10!");
+
+} else {
+    
+    document.write("You are not over 10!");
+}
