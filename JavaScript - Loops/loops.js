5cafc8b547960695725eff15ee98d0ff7380ba32
trevkat <trevor.katilius@icloud.com>
2024-01-04 10:31:10 -0600

    JavaScript - For Loops versus While Loops

--- JavaScript - Loops/loops.js
+++ JavaScript - Loops/loops.js
@@ -14,4 +14,52 @@ document.write("you are now over 10");
 
 */
 
-// For Loops
+// For Loop - Same as while Loop above but written differently
+
+/*
+
+for (age = 5; age < 10; age++) {
+        
+    console.log("Your age is less than 10");
+    
+}
+
+document.write("you are now over 10");
+
+*/
+
+// Using a For Loop to update link tags
+
+/*
+
+var links = document.getElementsByTagName("a");
+
+for (i = 0; i <= links.length; i++) {
+        
+    console.log("This is link number:" + i);
+    
+}
+
+document.write("All links are now looped");
+
+*/
+
+// For Loop - Break & Continue
+
+for (i = 0; i < 10; i++) {
+    
+    if(i === 5 || i === 3){
+        
+        continue;
+    }
+
+    console.log(i);
+
+    if (i === 7) {
+
+        break;
+    }
+
+}
+
+console.log("I have broken out of the loop");
