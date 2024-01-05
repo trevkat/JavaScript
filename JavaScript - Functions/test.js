f55957b3085d70d871a8fcc2525718010e590f1b
trevkat <trevor.katilius@icloud.com>
2024-01-05 11:59:41 -0600

    More work with JS Functions

---
+++ JavaScript - Functions/test.js
@@ -0,0 +1,12 @@
+function getAverage (a,b){
+    
+    
+    var average = (a + b) / 2;
+    console.log(average);
+    return average
+    
+}
+
+var myResult = getAverage(7,12);  //pass in numbers for a,b
+
+console.log("the average is " + myResult);
