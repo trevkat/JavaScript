efb691657d763b91782487e714f128a2b8849170
trevkat <trevor.katilius@icloud.com>
2024-01-06 12:10:35 -0600

    Update test.js

--- JavaScript - Functions/test.js
+++ JavaScript - Functions/test.js
@@ -1,12 +1,11 @@
 function getAverage (a,b){
     
-    
-    var average = (a + b) / 2;
+    var average = (a + b) / 2; // This is a Local Variable that can only be used within this Function. Average cannot be called outside of the Function.
     console.log(average);
     return average
     
 }
 
-var myResult = getAverage(7,12);  //pass in numbers for a,b
+var myResult = getAverage(7,12);  //pass in numbers for a,b  -This is also a Global Variable which can be used anywhere
 
 console.log("the average is " + myResult);
