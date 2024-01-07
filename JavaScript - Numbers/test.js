11d275821e9af207d4d01c2d5c7836bc13fb9158
trevkat <trevor.katilius@icloud.com>
2024-01-07 13:07:36 -0600

    NaN

--- JavaScript - Numbers/test.js
+++ JavaScript - Numbers/test.js
@@ -1,6 +1,7 @@
 var a = 5;
 var b = 5;
 
+
 console.log(a + b);
 console.log(typeof (a + b) ); //number
 
@@ -8,3 +9,18 @@ console.log(Math.round(7.8)) //rounds the number
 console.log(Math.floor(7.8)) //rounds down
 console.log(Math.ceil(7.8))  //rounds up
 console.log(Math.PI) //logs PI to the console
+
+
+var c = "apple"; // This is NaN
+
+if (isNaN(c)){
+
+    console.log("This is not a number");
+
+} else {
+
+    console.log("This IS a number!!");
+
+}
+
+// End of checking for NaN
