dccb50ac845786d545d6c8c12dbe932ed9b4ad90
trevkat <trevor.katilius@icloud.com>
2024-01-16 15:40:58 -0600

    Changing element attributes with JS

--- JavaScript - DOM/script.js
+++ JavaScript - DOM/script.js
@@ -15,4 +15,17 @@ myTitle.textContent;
 
 myTitle.textContent = "Updated via JavaScript";
 
+// Accessing and changing attributes ** Remember to add these to the console
 
+var link = document.getElementById("test")
+link.getAttribute("href"); // used to get the attribute 
+
+link.setAttribute("class", "pie") // used to update the attribute. Second parameter is what its updated to. 
+
+link.setAttribute("alt", "hello") // Can also add an attribute
+
+link.className;
+
+link.className("ninja") // updates from pie back to ninja
+
+link.style
